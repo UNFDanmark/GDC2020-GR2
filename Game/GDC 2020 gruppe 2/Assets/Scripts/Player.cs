5b3ddf47522e68538349af5c23d25240f33ccabf
Daniel <danysvids@gmail.com>
2020-07-28 17:18:51 +0200

    Tilføjer gamet

---
+++ Game/GDC 2020 gruppe 2/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("a"))
+        { rb.AddForce(-500 * Time.deltaTime, 0, 0); }
+
+        else if (Input.GetKey("d"))
+        { rb.AddForce(500 * Time.deltaTime, 0, 0); }
+
+       
+    }
+}
+
