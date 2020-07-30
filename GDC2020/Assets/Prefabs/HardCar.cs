9205891788ecf5459b51199e5404dc101dfd81be
Daniel <danysvids@gmail.com>
2020-07-30 22:19:13 +0200

    Our game Broke

---
+++ GDC2020/Assets/Prefabs/HardCar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public Rigidbody Car;
+    public float carSpeed = 3;
+    public float destroytime = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, destroytime);
+
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Car.velocity = new Vector3(0, 0, -carSpeed);
+
+    }
+}
