ab90251e98ca13cbfaa8c4e9dfc0483fcf55ee9c
Daniel <danysvids@gmail.com>
2020-07-31 10:20:53 +0200

    Nye Ting

---
+++ GDC2020/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public  Vector3 offset;
+    public float previousZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (previousZ - player.position.z > 0)
+        {
+            transform.position = new Vector3(offset.x, offset.y, player.position.z + offset.z);
+            print("hello");
+        }
+        previousZ = player.position.z;
+    }
+}
